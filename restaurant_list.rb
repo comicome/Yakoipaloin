1a8c6f0d9873adb6610773c5eee5252db74d02ff
Côme Delaval <come@mbp-de-come.home>
2018-11-21 11:36:17 +0100

    parse and store all the restaurants from the url in json and csv

--- restaurant_list.rb
+++ restaurant_list.rb
@@ -2,37 +2,63 @@ require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
 require 'json/ext'
+require 'csv'
 
+def parse_restaurants(nombreRestaurants, restaurants_list)
 
-doc = Nokogiri::HTML(open("https://www.tripadvisor.fr/RestaurantSearch?Action=AVAIL_CLEAR&geo=187147&ajax=1&zfp=10598%2C10601&zfn=7236772&itags=10591&sortOrder=relevance&availSearchEnabled=true&eaterydate=2018_11_20&date=2018-11-21&time=20%3A00%3A00&people=2"), nil, Encoding::UTF_8.to_s)
+    url = "https://www.tripadvisor.fr/RestaurantSearch?Action=PAGE&geo=187147&ajax=1&zfp=10598,10601&zfn=7236772&itags=10591&sortOrder=relevance&o=a#{nombreRestaurants}&availSearchEnabled=true&time=12:00:00"
+    doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
 
-restaurants_list = []
+    doc.css("div.listing div.ui_column").each do |restaurant|
+        types_list = []
+        if restaurant.css('div.title a').text != ''
 
-doc.css("div.listing div.ui_column").each do |restaurant|
-    types_list = []
-    if restaurant.css('div.title a').text != ''
+            rating = restaurant.css('div.rating span').to_s.match(/_(\d+)/)
 
-        rating = restaurant.css('div.rating span').to_s.match(/_(\d+)/)
+            restaurant.css('a.item.cuisine').each do |cuisine|
+                type = cuisine.text
+                types_list << type
+            end
 
-        restaurant.css('a.item.cuisine').each do |cuisine|
-            type = cuisine.text
-            types_list << type
+            restaurant = {
+                "name" => restaurant.css('div.title a').text.gsub("\n", ''),
+                "reviewCount" => restaurant.css('span.reviewCount a').text.gsub("\n", '').gsub(" avis ", '').to_i,
+                "price" => restaurant.css('span.item.price').text,
+                "type_list" => types_list,
+                "rating" => rating[1..-1].first.to_f/10,
+                "source" => "Tripadvisor"
+            }
+            restaurants_list << restaurant
+            nombreRestaurants += 1
         end
+    end
+    if nombreRestaurants % 30 == 0
+        parse_restaurants(nombreRestaurants, restaurants_list)
+    end
+    return restaurants_list
+end
 
-        restaurant = {
-            "name" => restaurant.css('div.title a').text.gsub("\n", ''),
-            "reviewCount" => restaurant.css('span.reviewCount a').text.gsub("\n", '').gsub(" avis ", '').to_i,
-            "price" => restaurant.css('span.item.price').text,
-            "type_list" => types_list,
-            "rating" => rating[1..-1].first.to_f/10,
-            "source" => "Tripadvisor"
-        }
-        restaurants_list << restaurant
+
+def hash_to_json_file (restaurants_list)
+    File.open("data/tripAdvisor.json","w") do |f|
+        f.write(restaurants_list.to_json)
     end
 end
-puts restaurants_list.to_json
 
-File.open("data/tripAdvisor.json","w") do |f|
-    f.write(restaurants_list.to_json)
-  end
 
+def hash_to_csv_file (restaurants_list)
+    column_names = restaurants_list.first.keys
+    s=CSV.generate do |csv|
+        csv << column_names
+        restaurants_list.each do |x|
+            csv << x.values
+        end
+    end
+    File.write('data/tripAdvisor.csv', s)
+end
+
+
+hash_to_convert = parse_restaurants(0, [])
+
+hash_to_csv_file(hash_to_convert)
+hash_to_json_file(hash_to_convert)
